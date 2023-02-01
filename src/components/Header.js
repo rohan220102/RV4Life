20e4634f8a3382ba4830712d678aa7d8af492ff8
Quynh <qtd.nguyen16@gmail.com>
2023-02-01 15:32:05 -0600

    Change href of RVLife title to RVLife.com

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
-import '../css/header.css';
+import "../css/header.css";
 
 const Header = () => {
   return (
     <div className="topnav" tabIndex={-1}>
-      <a className="active" href="#home" id="rvlife" tabIndex={-1}>RVLIFE</a>
-      <a className="active" href="#home" id="planner" tabIndex={-1}>PLANNER</a>
+      <a
+        className="active"
+        href="https://rvlife.com/"
+        id="rvlife"
+        tabIndex={-1}
+      >
+        RVLIFE
+      </a>
+      <a className="active" href="#home" id="planner" tabIndex={-1}>
+        PLANNER
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
