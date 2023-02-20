36677ec685a54773d18c8632e19c5301e472c8ad
Quynh <qtd.nguyen16@gmail.com>
2023-02-20 14:39:22 -0600

    Remove hardcoded data in frontend app

--- frontend/src/functions/search.js
+++ frontend/src/functions/search.js
@@ -1,8 +1,5 @@
-import getResults from "./getResults";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoicXV5bmgxNiIsImEiOiJjbGI3anljd2QwYno1M3ZtcjhmeWwxNzk0In0.F6D6mGrZ1-0tjVTDPiMgig";
+// url of api server
+const server = "http://127.0.0.1:8000";
 
 // this function should fetch and maybe process the results
 const search = (input, setResults, startContext) => {
@@ -10,13 +7,12 @@ const search = (input, setResults, startContext) => {
   const lon = input.input.geometry.coordinates[0];
   const lat = input.input.geometry.coordinates[1];
   const { start, setStart } = startContext;
+
   // setStart(input.input.geometry.coordinates);
   setStart([-77.068444, 38.909664]);
-  // const results = getResults(input);
-  // const results = JSON.parse(res);
-  // const url = `http://127.0.0.1:8000/server/next_stops/${input}`;
-  const url = `http://127.0.0.1:8000/api/search/?loc=${lon},${lat}`;
 
+  // fetch results from server
+  const url = `${server}/api/search/?loc=${lon},${lat}`;
   fetch(url)
     .then((response) => response.json())
     .then((json) => {
@@ -24,7 +20,6 @@ const search = (input, setResults, startContext) => {
       console.log("Fetched data from server:");
       console.log(json);
     });
-  // setResults(results);
 };
 
 export default search;
