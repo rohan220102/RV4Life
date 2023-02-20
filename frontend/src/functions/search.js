202bc07905e30f32c0b1366623ecb415579c782a
Quynh <qtd.nguyen16@gmail.com>
2023-02-19 23:50:37 -0600

    CHange cursor to point when hovering on marker

--- frontend/src/functions/search.js
+++ frontend/src/functions/search.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken =
 
 // this function should fetch and maybe process the results
 export default function search(input, setResults, startContext) {
-  console.log(input);
+  console.log("Searching...");
   const { start, setStart } = startContext;
   // setStart(input.input.geometry.coordinates);
   setStart([-77.068444, 38.909664]);
@@ -16,6 +16,9 @@ export default function search(input, setResults, startContext) {
     "https://raw.githubusercontent.com/quynh16/quynh16.github.io/main/results.json"
   )
     .then((response) => response.json())
-    .then((json) => setResults(json));
+    .then((json) => {
+      setResults(json);
+      console.log(json);
+    });
   // setResults(results);
 }
